6d28f568e4625c27a6f83592e0db21b4f6c660b6
William McClintock <wmcclintock89@gmail.com>
2017-08-19 12:13:32 +0100

    created new file "cachevector.R"
    example of cacheing computatioanllly expensive results and recalling

---
+++ cachevector.R
@@ -0,0 +1,34 @@
+## Put comments here that give an overall description of what your
+## functions do
+
+## Write a short comment describing this function
+
+makeVector <- function(x = numeric()) {
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setmean <- function(mean) m <<- mean
+        getmean <- function() m
+        list(set = set, get = get,
+             setmean = setmean,
+             getmean = getmean)
+}
+
+
+## Write a short comment describing this function
+
+cacheVector <- function(x, ...) {
+        ## Return a matrix that is the inverse of 'x'
+        m <- x$getmean()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- mean(data, ...)
+        x$setmean(m)
+        m
+}
